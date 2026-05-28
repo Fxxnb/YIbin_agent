10248e27076927934840acc3fe3d150cdf07ff7a
Fxxnb <161283062+Fxxnb@users.noreply.github.com>
2026-05-28 11:22:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -78,5 +78,5 @@ def chat():
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
+        port=int(os.environ.get("PORT", 8080))
     )
